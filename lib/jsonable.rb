0204ba072fce931cfe777c78e044d96eb7b04b25
kinjouj <kinjouj@gmail.com>
2025-05-30 21:14:38 +0900

    update

--- lib/jsonable.rb
+++ lib/jsonable.rb
@@ -1,14 +1,17 @@
 # frozen_string_literal: true
 
 module Jsonable
-  def to_json(*)
+  def to_hash
     excludes = exclude_jsonable || []
     hash = self.instance_variables.map {|key|
       keystr = key.to_s.tr("@", "")
       next if excludes.include?(keystr)
       [ keystr, self.instance_variable_get(key)]
     }.compact.to_h
-    hash.to_json
+  end
+
+  def to_json(*)
+    to_hash.to_json
   end
 
   def exclude_jsonable
